7e7a2300d5c77f437110497493c40161ac012869
William Luck <luck.william.h@gmail.com>
2022-03-28 13:33:33 -0500

    commit tree 04d42356dea573fd4081a12c8d38d2b0bd528ec9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');  // grabs form
+
+  inputForm.addEventListener('submit', event => {  // adds event listener to the submit button, the following is the cb function
+      event.preventDefault() // prevents referesh of the page 
+      const input = document.querySelector('input#searchByID')  // saves user input to input variable
+
+      console.log(input.value)  //debug
+
+      fetch(`http://localhost:3000/movies/${input.value}`)  // retrives data from server, using user input
+        .then(response => response.json()) // conerts to json object 
+        .then(data => { // another callback function, data is what is returned from the local server 
+            const title = document.querySelector('section#movieDetails h4');  
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;  // updates title based on data retrived from server 
+            summary.innerText = data.summary; // same 
+        })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
